6599b62dc33ebc6a9c4731aaebb00c0e2a1126d9
Coleman O'Phelan <ophelanc@bc.edu>
2013-07-22 15:04:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect('localhost', 'root', 'root', 'brickles');
+$con = mysqli_connect('localhost', '*********', '*************', 'brickles');
 if (mysqli_connect_errno($con))
 {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
