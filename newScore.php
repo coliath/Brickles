40690cb69df73e230626072b2952aa6282e27e9c
Martín Aranciaga <rainbowtupperware@openmailbox.org>
2015-06-06 20:34:18 +0000

    SQL injection fixed

--- newScore.php
+++ newScore.php
@@ -1,7 +1,7 @@
 <?php
 $con = mysqli_connect('localhost', 'root', 'root', 'brickles');
 if (isset($_GET['player'])) {
-    $sql = "INSERT INTO scores VALUES ('$_GET[player]','$_GET[score]')";
+    $sql = "INSERT INTO scores VALUES ('".mysqli_escape_string($_GET['player'])."','".mysqli_escape_string($_GET['score'])."')";
     mysqli_query($con, $sql);
 }
 ?>
